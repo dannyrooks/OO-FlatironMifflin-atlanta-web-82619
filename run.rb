adc7e22a9c075ee8460ee15b9e36742b8999fd0c
Daniel Rooker <dannyrooks@gmail.com>
2019-09-03 16:33:58 -0400

    Done.

--- run.rb
+++ run.rb
@@ -1,9 +1,24 @@
 require_relative "lib/Manager"
 require_relative "lib/Employee"
+require_relative "lib/Department"
 require 'pry'
 
+m1 = Manager.new("Dwight", 40)
+m2 = Manager.new("Oscar", 37)
+m3 = Manager.new("Kelly", 35)
+m4 = Manager.new("Kevin", 42)
 
-#Test your code here
+e1 = Employee.new("Raza", 90000)
+e2 = Employee.new("Dan", 91000)
+e3 = Employee.new("Alex", 120000)
+e4 = Employee.new("Nick", 40000)
+e5 = Employee.new("Andrea", 89000)
+
+d1 = Department.new("Sales", m1, e1)
+d2 = Department.new("Accounting", m2, e3)
+d3 = Department.new("Customer Service", m3, e2)
+d4 = Department.new("Marketing", m4, e5)
+
 
 
 binding.pry
